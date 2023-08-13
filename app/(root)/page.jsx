622faf486a43499661642982b084922a51c3b890
ziya yildirim <zia.yildirimm@gmail.com>
2023-08-13 16:52:40 +0300

    Clerk Authentication (Admin)

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -1,10 +1,13 @@
-import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+
+const SetupPage = () => {
     return (
-        <div>
-            <Button> Click me </Button>
+        <div className='p-4'>
+           <UserButton afterSignOutUrl="/"/>
         </div>
     )
   }
   
+
+  export default  SetupPage;
